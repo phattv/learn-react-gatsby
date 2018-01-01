312de9860b12c4596bf3460c2b176ae22f1d80e9
Phat Tran <PHAT@Phattv.local>
2018-01-01 23:47:04 +0700

    Override Header component default styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,35 +4,36 @@ import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
 import './index.css'
+import '../styles/layout-override.css'
 
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: '#f5f5f5',
+      marginBottom: '3rem',
+      borderBottom: '2px solid #e6e6e6',
     }}
   >
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
+        maxWidth: 980,
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
+      <h1 style={{margin: 0, textAlign: 'center',fontSize: '18px'}}>
+        <Link to="/"
+              style={{
+                color: 'black',
+                textDecoration: 'none',
+              }}
         >
-          Gatsby
+          CodeStack
         </Link>
       </h1>
     </div>
   </div>
-)
+);
 
 const TemplateWrapper = ({ children }) => (
   <div>
